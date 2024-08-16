8e124aa07afbbf06bc0a9b3dfa0bae076e757781
abhicsng007 <abhicsng007@gmail.com>
2024-08-16 19:06:52 +0530

    updated fetch transcript

--- backend/fetch_transcript.py
+++ backend/fetch_transcript.py
@@ -1,16 +1,21 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import sys
 
-
-
 def transcribe(data):
     transcript = ''
     for value in data:
-      for key,val in value.items():
-        
-        if key=='text':
-          transcript += val+' '
+        if 'text' in value:
+            transcript += value['text'] + ' '
     return transcript
 
+# Corrected: Pass proxies to the get_transcript method
+proxies = {
+    "http": "http://proxy.goproxy.com:30000",
+    "https": "http://proxy.goproxy.com:30000"
+}
 
-print(transcribe(YouTubeTranscriptApi.get_transcript(sys.argv[1]) , proxies = {"http": "http://proxy.goproxy.com:30000", "https": "http://proxy.goproxy.com:30000"}))
+try:
+    transcript_data = YouTubeTranscriptApi.get_transcript(sys.argv[1], proxies=proxies)
+    print(transcribe(transcript_data))
+except Exception as e:
+    print(f"An error occurred: {e}")
