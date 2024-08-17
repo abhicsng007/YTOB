6140cc6308ffbcf0b573afcc8f665c4b2a4e3418
abhicsng007 <abhicsng007@gmail.com>
2024-08-17 19:33:14 +0530

    modifying landing page

--- client/ytob/app/(landing)/page.js
+++ client/ytob/app/(landing)/page.js
@@ -1,10 +1,24 @@
+"use client";
+
 import LandingContent from "@/components/ui/LandingContent";
 import LandingFooter from "@/components/ui/LandingFooter";
 import LandingHero from "@/components/ui/LandingHero";
 import LandingNavbar from "@/components/ui/LandingNavbar";
+import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 
 function LandingPage() {
+ const { isSignedIn } = useAuth();
+ const router = useRouter();
+
+ useEffect(() => {
+  if(isSignedIn){
+    router.push('/dashboard');
+  }
+ },[]);
+
   return (
     <div >
             <LandingNavbar/>
