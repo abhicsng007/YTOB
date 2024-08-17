3461f43fbe6ba4110d901d68f35fb698c4efd41f
abhicsng007 <abhicsng007@gmail.com>
2024-08-17 13:48:18 +0530

    modified auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { signup, login, refreshAccessToken , validateToken , changePassword } from '../controllers/authController.js';
+import { signup, login, logout, refreshAccessToken , validateToken , changePassword } from '../controllers/authController.js';
 
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/logout', logout);
 router.post('/refresh', refreshAccessToken); // New endpoint for refreshing tokens
 router.post('/validate', validateToken);
 router.post('/change-password', changePassword);
