b33b42abb4b7417acca8e61383661b528b0fb523
abhicsng007 <abhicsng007@gmail.com>
2024-08-18 21:03:36 +0530

    added blog  page

---
+++ client/ytob/app/(landing)/blog/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Blog() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+        <h1 className="text-4xl font-bold mb-6">Our Blog</h1>
+        <p className="text-lg text-gray-400">Coming Soon...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Blog;
