62c28db1a76dec0e609c9767701d8cd07b4833f9
abhicsng007 <abhicsng007@gmail.com>
2024-08-19 14:21:21 +0530

    modified next config

--- client/ytob/next.config.mjs
+++ client/ytob/next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost'],
-    },
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: 'localhost',
+          },
+          {
+            protocol: 'https',
+            hostname: 'vercel.com',
+          },
+        ],
+      },
 
 };
 
